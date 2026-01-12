9d4086dcbf748f632227d41e78466dfce836b0d0
Claude Joseph Manbodh Jr. 'Joe <63468241+cjmDevelop@users.noreply.github.com>
2026-01-12 15:57:52 -0500

    Added deal button right below cards, gonna make deal just refresh window

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ const cards = document.querySelectorAll(".card");
 cards.forEach((card) => {
   card.addEventListener("click", (e) => e.target.className.includes("card")? e.target.classList.toggle("selected") : e.target.closest(".card").classList.toggle("selected"));
 });
+//-----------------------------------------------------------------------
 
 let hands = "A 2 3 4 5 6 7 8 9 10 J Q K";
 hands = hands.split(" ")
 
+const firstCardValue = document.getElementById("first-card-value")
+const secondCardValue = document.getElementById("second-card-value")
 
-const firstCard = document.getElementById("first-card")
-const secondCard = document.getElementById("second-card")
+firstCardValue.textContent = hands[Math.floor(Math.random() * hands.length)];
+secondCardValue.textContent = hands[Math.floor(Math.random() * hands.length)];
+//-----------------------------------------------------------------------
 
-firstCard.textContent = hands[Math.floor(Math.random() * hands.length)];
-secondCard.textContent = hands[Math.floor(Math.random() * hands.length)];
-
-//To do: add number for percentage in UI
+const suits = ['❤️', '♣️', '♦️', '♠️']
+let firstCardSuit = document.getElementById("first-card-suit")
+let secondCardSuit = document.getElementById("second-card-suit");
+firstCardSuit.textContent = suits[Math.floor(Math.random() * suits.length)];
+secondCardSuit.textContent = suits[Math.floor(Math.random() * suits.length)];
