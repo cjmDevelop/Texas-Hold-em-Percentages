1ff2135cd0950d3269035707143923ff58b86015
Claude Joseph Manbodh Jr. 'Joe <63468241+cjmDevelop@users.noreply.github.com>
2026-01-12 12:00:43 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const cards = document.querySelectorAll(".card");
+cards.forEach((card) => {
+  card.addEventListener("click", (e) => e.target.className.includes("card")? e.target.classList.toggle("selected") : e.target.closest(".card").classList.toggle("selected"));
+});
+
+let hands = "A 2 3 4 5 6 7 8 9 10 J Q K";
+hands = hands.split(" ")
+// console.log(hands.length)
+// console.log(Math.floor(Math.random() * hands.length) + 1)
+
+
+
+const firstCard = document.getElementById("first-card")
+const secondCard = document.getElementById("second-card")
+
+firstCard.textContent = hands[Math.floor(Math.random() * hands.length) + 1];
